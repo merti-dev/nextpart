395a381c2037f67a751bd054dab16acfccaacfb8
ali mert inal <mert.inal@gmail.com>
2025-04-09 13:36:28 +0200

    navBar component added

---
+++ components/ui/nav-bar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className='w-full border-0 py-4 lg:px-24 px-10 bg-gray-950'>
+<h1 className='text-3xl text-white'>
+    <a href="/" >NextNav</a>
+
+</h1>
+    </nav>
+    </>
+  )
+}
